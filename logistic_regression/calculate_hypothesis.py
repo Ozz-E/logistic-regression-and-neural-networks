f8959f87ac7e01ed27999b075da793a953ee69fb
Ozz-E <67159787+Ozz-E@users.noreply.github.com>
2023-02-02 13:22:00 +0000

    initial repo commit

---
+++ logistic_regression/calculate_hypothesis.py
@@ -0,0 +1,17 @@
+import numpy as np
+from sigmoid import *
+
+def calculate_hypothesis(X, theta, i):
+    """
+        :param X            : 2D array of our dataset
+        :param theta        : 1D array of the trainable parameters
+        :param i            : scalar, index of current training sample's row
+    """
+    hypothesis = 0.0
+
+    #hypothesis = np.dot(X[i], theta)
+    hypothesis = np.matmul(X[i], theta)
+    
+    result = sigmoid(hypothesis)
+
+    return result
