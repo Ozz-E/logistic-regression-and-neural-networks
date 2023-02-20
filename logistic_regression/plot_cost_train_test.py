df37bbd5d1726b4f47567283bb7ca085a547a296
Ozz-E <67159787+Ozz-E@users.noreply.github.com>
2023-02-20 13:05:52 +0000

    Update plot_cost_train_test.py

--- logistic_regression/plot_cost_train_test.py
+++ logistic_regression/plot_cost_train_test.py
@@ -2,6 +2,12 @@ import matplotlib.pyplot as plt
 import os
 
 def plot_cost_train_test(train_cost, test_cost, ax1):
+
+    """ This function plots the training and test cost of the neural network.
+
+    """    
+
+    ax1.set_title('Cost vs Iterations')
     
     ax1.set_xlabel('Iterations')
     ax1.set_ylabel('Cost')
